ca154a88bbad1d27309697240a2386463ff031f3
Abel C Dixon <abelcheruvathoor@gmail.com>
2019-11-29 10:23:41 +0530

    Basic Network Image Displaying

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("I am Rich"),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          backgroundColor: Colors.blue,
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://www.w3schools.com/w3css/img_lights.jpg'),
+            ),
+          ),
+        ),
+      ),
+    );
